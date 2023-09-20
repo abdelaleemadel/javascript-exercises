1c94f2551b0ccfe347d1605acce59b2b7478e9ca
AbdelaleemAhmed <ahmedadel2912@gmail.com>
2023-09-20 22:49:35 +0300

    Solve Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,22 @@
-const add = function() {
-	
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const sum = arr => arr.reduce((total, element) => total + element, 0);
+
+
+const multiply = arr => arr.reduce((total, element) => total * element);
+
+const power = (a, b) => a ** b;
+
+const factorial = function (number) {
+  if (number == 0 || number == 1) {
+    return 1
+  } else {
+    return number * factorial(number - 1)
+  }
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
