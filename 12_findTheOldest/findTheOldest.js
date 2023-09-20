90880739279ad6bfd22ebe917a89ea3621e9e9a1
AbdelaleemAhmed <ahmedadel2912@gmail.com>
2023-09-21 00:04:50 +0300

    Solve findTheOldest challenge

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = people => {
+    const sorted = people.sort(function (firstPerson, secondPerson) {
+        const thisYear = new Date().getFullYear();
+        return (((secondPerson.yearOfDeath || thisYear) - secondPerson.yearOfBirth) - ((firstPerson.yearOfDeath || thisYear) - firstPerson.yearOfBirth))
+    })
+    return sorted[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
