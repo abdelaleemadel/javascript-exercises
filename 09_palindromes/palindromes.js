558e695478c42dd1524d5880689cc50d5d0dda1a
AbdelaleemAhmed <ahmedadel2912@gmail.com>
2023-09-20 23:09:57 +0300

    Solve palindromes challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
-const palindromes = function () {
-
-};
-
+const palindromes = string => {
+    const properString = string.toLowerCase().replace(/[^a-z0-9]/g, "")
+    return properString === properString.split("").reverse().join("")
+}
 // Do not edit below this line
 module.exports = palindromes;
