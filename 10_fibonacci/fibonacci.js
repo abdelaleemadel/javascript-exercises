f88f2960676539a69c124aad7e1fff6742b83028
AbdelaleemAhmed <ahmedadel2912@gmail.com>
2023-09-20 23:32:44 +0300

    Solve fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function (number) {
+    if (number < 1) { return "OOPS" }
+    else if (number == 1 || number == 2) {
+        return 1
+    } else {
+        let arr = [1, 1]
+        for (let i = 2; i < number; i++) {
+            arr.push(arr[i - 1] + arr[i - 2])
+        }
+        return arr[number - 1]
+    }
+}
+/* Recursive way takes so much time */
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+
+
+
+
